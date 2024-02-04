7b7da4c0f54411b782b9f1cb997ffb6a9ab9f91a
Zhihan Yue <zhihan.yue@pku.edu.cn>
2024-02-04 20:40:45 +0800

    Requiring pandas>=0.20.0

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages, Command, find_namespace_packages
+from setuptools import setup, find_packages, Command
 from setuptools.command.sdist import sdist
 from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
