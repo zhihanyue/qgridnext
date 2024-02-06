4798d05c1eb4b2ee1c3e67c339a77fbe07ee1722
Zhihan Yue <zhihan.yue@foxmail.com>
2024-02-06 17:59:00 +0800

    Correct the path

--- js/src/extension.js
+++ js/src/extension.js
@@ -7,7 +7,7 @@ if (window.require) {
     window.require.config({
         map: {
             "*" : {
-                "qgrid": "nbextensions/qgrid/index"
+                "qgrid": "nbextensions/qgridnext/index"
             }
         }
     });
