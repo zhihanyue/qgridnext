ebd6e43c77edcffe0e96bcd0643eb57bd6c38dfd
Zhihan Yue <zhihan.yue@foxmail.com>
2024-10-31 21:34:00 +0800

    Update test_envs

--- test_envs/launch_lab.sh
+++ test_envs/launch_lab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$0")"
-jupyter lab --ip 0.0.0.0 --port=39050 --notebook-dir=$SCRIPT_DIR --no-browser
+jupyter lab --ip 0.0.0.0 --notebook-dir=$SCRIPT_DIR --no-browser $*
--- test_envs/launch_voila.sh
+++ test_envs/launch_voila.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$0")"
-voila "$SCRIPT_DIR/example.ipynb" --port 39050 --no-browser --Voila.ip=0.0.0.0 --enable_nbextensions=True
+voila "$SCRIPT_DIR/example.ipynb" --no-browser --Voila.ip=0.0.0.0 --enable_nbextensions=True $*
